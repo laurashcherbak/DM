75fcd3702ae5685f7592df1db7169ee758ff09b1
Laura Shcherbak <laura.shcherbak@gmail.com>
2021-01-29 11:51:50 +0200

    Added Final test questions & answers (44.33 of 55.00 ~ 80% passed)

---
+++ Lab6/Lab.cpp
@@ -0,0 +1,119 @@
+﻿// Lab 6
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <io.h>
+#include <fcntl.h>
+
+using namespace std;
+
+int main()
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+
+    wcout << L"Lab 6" << endl << endl;
+    int R1[5][2] = { {1, 2}, {1, 3}, {1, 4}, {2, 3}, {4, 3} };
+    int A[4] = { 1, 2, 3, 4 };
+
+    wcout << " R1 = { ";
+    for (int i = 0; i < 5; i++) {
+        wcout << "(" << setw(2) << R1[i][0] << "," << setw(2) << R1[i][1] << ")";
+        if (i == 4)
+            wcout << " ";
+        else
+            wcout << ", ";
+    }
+    wcout << "}" << endl << endl;
+
+    wcout << " A = {";
+    for (int i = 0; i < 4; i++) {
+        wcout << setw(2) << A[i];
+        if (i == 3)
+            wcout << " ";
+        else
+            wcout << ", ";
+    }
+    wcout << "}" << endl << endl;
+
+    wcout << " A^2 = { ";
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            wcout << "(" << setw(2) << A[i] << "," << setw(2) << A[j] << ")";
+            if (i == 3 && j == 3)
+                wcout << " ";
+            else
+            wcout << ", ";
+        }
+    }
+    wcout << "}" << endl << endl;
+
+    wcout << L"Задати відношення за допомогою матриці та графа." << endl << endl;
+
+    int x = 0, y = 0;
+    wcout << setw(4) << " \ " << setw(4) << "1" << setw(4) << "2" << setw(4) << "3" << setw(4) << "4" << endl;
+    for (int i = 0; i < 4; i++) {
+        wcout << setw(4) << i + 1;
+        for (int j = 0; j < 4; j++) {
+            if (R1[x][0] == i + 1 && R1[x][1] == j + 1) {
+                wcout << setw(4) << 1;
+                x++;
+            }
+            else {
+                wcout << setw(4) << 0;
+            }
+        }
+        wcout << endl;
+    }
+    wcout << endl;
+
+    wcout << "1    2" << endl;
+    wcout << "*----*" << endl;
+    wcout << "|\\  /" << endl;
+    wcout << "| \\/ " << endl;
+    wcout << "| /\\ " << endl;
+    wcout << "|/  \\" << endl;
+    wcout << "*----*" << endl;
+    wcout << "3    4" << endl;
+    wcout << endl;
+
+    int v1 = 0, v3 = 0, v4 = 0, v5 = 0, v6 = 0;
+    int j;
+    for (int i = 0; i < 5; i++) {
+        j = 0;
+        if (R1[i][0] == R1[i][1]) {
+            v1++;
+        }
+        while (j < 5) {
+            if (R1[i][0] == R1[j][1] && R1[i][1] == R1[j][0])
+                v3++;
+            j++;
+        }
+    }
+
+    wcout << L"Відношення R1 на множині A^2 є : " << endl;
+    if (v1 == 4)
+        wcout << L" - рефлексивне" << endl;
+    else 
+        wcout << L" - не рефлексивне" << endl;
+    if (v1 == 0)
+        wcout << L" - іррефлексивне" << endl;
+    else
+        wcout << L" - не іррефлексивне" << endl;
+    if ((v3 - v1) == 5)
+        wcout << L" - симетричне" << endl;
+    else
+        wcout << L" - не симетричне" << endl;
+    if ((v3 - v1) == 0)
+        wcout << L" - антисиметричне" << endl;
+    else
+        wcout << L" - не антисиметричне" << endl;
+    if (v3 == 0)
+        wcout << L" - асиметричне" << endl;
+    else
+        wcout << L" - не асиметричне" << endl;
+    wcout << L" - транзитивне" << endl;
+
+    return 0;
+}
---
+++ Lab7/Lab.cpp
@@ -0,0 +1,128 @@
+﻿#ifndef _CRT_SECURE_NO_WARNINGS
+    #define _CRT_SECURE_NO_WARNINGS
+#endif // !#define _CRT_SECURE_NO_WARNINGS
+
+// Lab 6
+#include <iostream>
+#include <iomanip>
+#include <string>
+//#include <io.h>
+//#include <fcntl.h>
+
+//#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+//#include <string>
+
+using namespace std;
+
+typedef string Info;
+struct Elem
+{
+    Elem* link1;
+    Elem* link2;
+    Elem* link3;
+    Elem* link4;
+    Elem* link5;
+    Info info;
+};
+
+void PrintTree(Elem* root, int level)
+{
+    if (root != NULL)
+    {
+        PrintTree(root->link1, level + 1);
+        PrintTree(root->link2, level + 1);
+        for (int i = 1; i <= level; i++)
+            cout << " ";
+        cout << root->info << endl;
+        PrintTree(root->link3, level + 1);
+        PrintTree(root->link4, level + 1);
+        PrintTree(root->link5, level + 1);
+    }
+}
+
+void Task1()
+{
+    Elem* p;
+    p = new Elem;
+    p->info = 'E';
+    p->link1 = new Elem;
+    p->link1->info = '(';
+    p->link2 = new Elem;
+    p->link2->info = 'E';
+    p->link2->link1 = new Elem;
+    p->link2->link1->info = 'C';
+    p->link2->link1->link1 = new Elem;
+    p->link2->link1->link1->info = '2';
+    p->link3 = new Elem;
+    p->link3->info = '*';
+    p->link4 = new Elem;
+    p->link4->info = 'E';
+    p->link4->link1 = new Elem;
+    p->link4->link1->info = '(';
+    p->link4->link2 = new Elem;
+    p->link4->link2->info = 'E';
+    p->link4->link2->link1 = new Elem;
+    p->link4->link2->link1->info = 'V';
+    p->link4->link2->link1->link1 = new Elem;
+    p->link4->link2->link1->link1->info = 'X';
+    p->link4->link3 = new Elem;
+    p->link4->link3->info = '+';
+    p->link4->link4 = new Elem;
+    p->link4->link4->info = 'E';
+    p->link4->link4->link1 = new Elem;
+    p->link4->link4->link1->info = 'E';
+    p->link4->link4->link1->link1 = new Elem;
+    p->link4->link4->link1->link1->info = 'V';
+    p->link4->link4->link1->link1->link1 = new Elem;
+    p->link4->link4->link1->link1->link1->info = 'Y';
+    p->link4->link4->link2 = new Elem;
+    p->link4->link4->link2->info = '+';
+    p->link4->link4->link3 = new Elem;
+    p->link4->link4->link3->info = 'E';
+    p->link4->link4->link3->link1 = new Elem;
+    p->link4->link4->link3->link1->info = 'C';
+    p->link4->link4->link3->link1->link1 = new Elem;
+    p->link4->link4->link3->link1->link1->info = '1';
+    p->link4->link5 = new Elem;
+    p->link4->link5->info = ')';
+    p->link5 = new Elem;
+    p->link5->info = ')';
+
+    //for (int i = 1; i < 8; i++) cout << " "; cout << p->link1->link1->link1->info << endl;
+    //for (int i = 1; i < 6; i++) cout << " "; cout << p->link1->link1->info << endl;
+    //for (int i = 1; i < 4; i++) cout << " "; cout << p->link1->info << endl;
+    //for (int i = 1; i < 2; i++) cout << " "; cout << "//" << endl;
+    ////for (int i = 1; i < 2; i++) cout << " "; cout << p->link2->info << endl;
+    //cout << p->info; cout << "-" << p->link2->info << endl;
+    //for (int i = 1; i < 2; i++) cout << " "; cout << "\\\\" << endl;
+    //for (int i = 1; i < 3; i++) cout << " "; cout << "\\" << p->link3->info << endl;
+
+    //PrintTree(p, 0);
+
+}
+
+void Task2()
+{
+    string A = "101";
+    string B = "010";
+    string S1 = "1";
+    string S2 = "0";
+
+    cout << "L(G) = { " << S1 << A << ", " << S2 << B << " }";
+}
+
+int main() 
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+
+    Task1();
+    Task2();
+
+    Count();
+
+    return 0;
+}
